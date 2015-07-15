ecbcb90e5963e8ea643ae7acf87099ce880a666e
Bertrand Lorent <blorent@softkinetic.com>
2015-07-15 16:10:11 +0200

    Add config file draft

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+return array(
+	'club' => 'VC Walhain',
+	'club_id' => 5083,
+	'servername' = 'mysql5-1.perso'
+	'username' => 'lorent_db',
+	'password' => 'vaSqt2FG',
+	'dbname' => 'lorent_db'
+);
+
+?>
